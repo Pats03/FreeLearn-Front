c43d58391727be0e6f7d7a84238c457ad990eb00
Pats03 <ananthateja2004@gmail.com>
2025-07-19 02:44:45 +0530

    doneee

--- src/utils/customFetch.js
+++ src/utils/customFetch.js
@@ -1,8 +1,4 @@
-import axios from 'axios';
-
 const customFetch = axios.create({
-  baseURL: import.meta.env.PROD ? import.meta.env.VITE_API_URL : '/api/v1',
+  baseURL: '/api/v1',
   withCredentials: true,
 });
-
-export default customFetch;
