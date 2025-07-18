3bd293b61bcf4d47cb689699c147e7e443a5bf3c
Pats03 <ananthateja2004@gmail.com>
2025-07-19 04:16:12 +0530

    yoooooooo

--- src/assets/wrappers/StatItem.js
+++ src/assets/wrappers/StatItem.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Wrapper = styled.article`
   padding: 2rem;
   background: var(--background-secondary-color);
-  border-bottom: 5px solid ${(props) => props.color};
+  border-bottom: 5px solid ${(props) => props.color || '#fff'};
   border-radius: var(--border-radius);
 
   header {
@@ -11,13 +11,15 @@ const Wrapper = styled.article`
     align-items: center;
     justify-content: space-between;
   }
+
   .count {
     display: block;
     font-weight: 700;
     font-size: 50px;
-    color: ${(props) => props.color};
+    color: ${(props) => props.color || '#fff'};
     line-height: 2;
   }
+
   .title {
     margin: 0;
     text-transform: capitalize;
@@ -25,18 +27,21 @@ const Wrapper = styled.article`
     text-align: left;
     margin-top: 0.5rem;
     font-size: 1.25rem;
+    color: #fff; /* bright title text */
   }
+
   .icon {
     width: 70px;
     height: 60px;
-    background: ${(props) => props.bcg};
+    background: ${(props) => props.bcg || '#444'};
     border-radius: var(--border-radius);
     display: flex;
     align-items: center;
     justify-content: center;
+
     svg {
       font-size: 2rem;
-      color: ${(props) => props.color};
+      color: ${(props) => props.color || '#fff'};
     }
   }
 `;
