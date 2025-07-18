2bde2ae85f5136ed5a490fa542c1c2ed51866a96
Pats03 <ananthateja2004@gmail.com>
2025-07-19 01:26:25 +0530

    cors

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5100/api',
+        target: 'https://free-learn-xi.vercel.app/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
